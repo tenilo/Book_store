fe7f6d862f53fd9a484a37ad4c82df5f58506308
yeo tenilo <teniloyacoubayeo@gmail.com>
2022-01-15 15:40:46 +0100

    first commit

---
+++ Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Book_WebApp.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Author { get; set; }
+        public string Name { get; set; }
+        public int Volume { get; set; }
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Book_WebApp.Startup))]
+namespace Book_WebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
